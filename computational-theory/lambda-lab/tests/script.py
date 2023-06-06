d0669fade26c78df9c0ce6ed0e35b5755fb89342
Tim Rolshud <tim.rolsh@gmail.com>
2023-06-06 12:03:54 -0400

    final tests works no extra credit

--- computational-theory/lambda-lab/tests/script.py
+++
@@ -1,9 +0,0 @@
-with open('./script.txt', 'r') as file:
-    lines = file.readlines()
-
-with open('./appendix-1-in.txt', 'w') as in_file, open('./appendix-1-out.txt', 'w') as out_file:
-    for index, line in enumerate(lines):
-        if index % 2 == 0:
-            in_file.write(line)
-        else:
-            out_file.write(line)
